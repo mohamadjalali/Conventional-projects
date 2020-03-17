3a51191fa9c83c327fbd42be5b288b662e8b126e
mohamadjalali <hawak.slardar@gmail.com>
2020-03-17 19:56:41 +0330

    Minor editing

--- FileTransfer/client.py
+++ FileTransfer/client.py
@@ -13,10 +13,10 @@ file_name = data.decode("ascii")
 print(f"Reciving File: {file_name}")
 time.sleep(0.5)
 
-with open(file_name, "wb") as writer:
+with open(file_name, "wb") as newfl:
     data = sock.recv(1024)
     while data:
-        writer.write(data)
+        newfl.write(data)
         data = sock.recv(1024)
     print("File Received Successfully!")
 sock.close()

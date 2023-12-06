52a8a97a38c3c8b685fb67f22147b63942dcb58a
mohamadjalali <mohamadjalali324@gmail.com>
2023-12-06 13:06:47 +0330

    write tests for other functions of inventory.py

--- tests/unit/test_resource.py
+++ tests/unit/test_resource.py
@@ -18,8 +18,8 @@ def resource(resource_values):
     return Resource(**resource_values)
 
 
+
 def test_create_resource(resource_values, resource):
-    print(resource.__dict__)
     for attr_name in resource_values:
         assert getattr(resource, attr_name) == resource_values[attr_name]
 
@@ -37,4 +37,104 @@ def test_create_invalid_allocated_type(resource_values, resource):
 def test_create_invalid_total_value(resource_values, resource):
     with pytest.raises(ValueError):
         Resource('Parrot', 'Pirates A-Hoy', -10, 4)
+
+
+@pytest.mark.parametrize("total,allocated", [(10, -5), (10, 20)])
+def test_create_invalid_allocated_value(total, allocated):
+    with pytest.raises(ValueError):
+        Resource('name', 'manu', total, allocated)
+
+
+def test_total(resource):
+    assert resource.total == resource._total
+
+
+def test_allocated(resource):
+    assert resource.allocated == resource._allocated
+
+
+def test_available(resource, resource_values):
+    assert resource.available == resource.total - resource.allocated
+    
+
+def test_categoryt(resource):
+    assert resource.category == 'resource'
+
+
+def test_str_repr(resource):
+    assert str(resource) == resource.name
+
+
+def test_repr_repr(resource):
+    assert repr(resource) == '{} ({} - {}): total={}, allocated={}'.format(
+        resource.name, resource.category, resource.manufacturer,
+        resource.total, resource.allocated
+    )
+
+
+def test_claim(resource):
+    n = 2
+    orginal_allocated = resource.allocated
+    orginal_available = resource.available
+    orginal_total = resource.total
+    resource.claim(n)
+    assert resource.allocated == orginal_allocated + n
+    assert resource.available == orginal_available - n
+    assert resource.total == orginal_total
+
+
+
+@pytest.mark.parametrize("value", [-1, 0, 1_500])
+def test_claim_invalid(resource, value):
+    with pytest.raises(ValueError):
+        resource.claim(value)
+
+
+
+def test_purchased(resource):
+    n = 2
+    orginal_total = resource.total
+    orginal_allocated = resource.allocated
+    resource.purchased(n)
+    assert resource.total == orginal_total + n
+    assert resource.allocated == orginal_allocated
+
+
+@pytest.mark.parametrize("value", [-1, 0])
+def test_purchased_invalid(resource, value):
+    with pytest.raises(ValueError):
+        resource.purchased(value)
+
+
+
+def test_freeup(resource):
+    n = 2
+    orginal_allocated = resource.allocated
+    orginal_total = resource.total
+    resource.freeup(n)
+    assert resource.allocated == orginal_allocated - n
+    assert resource.total == orginal_total
+
+
+
+@pytest.mark.parametrize("value", [-1, 0, 1_500])
+def test_freeup_invalid(resource, value):
+    with pytest.raises(ValueError):
+        resource.freeup(value)
+
+
+
+def test_died(resource):
+    n = 2
+    orginal_total = resource.total
+    orginal_allocated = resource.allocated
+    resource.died(n)
+    assert resource.total == orginal_total - n
+    assert resource.allocated == orginal_allocated - n
+
+
+@pytest.mark.parametrize("value", [-1, 0, 1_500])
+def test_died_invalid(resource, value):
+    with pytest.raises(ValueError):
+        resource.died(value)
 

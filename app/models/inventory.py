8c85a3e28ea9c7c11909321406c4d738bd4c23df
mohamadjalali <mohamadjalali324@gmail.com>
2023-12-12 00:38:24 +0330

    add capacity_gb method.

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -15,29 +15,9 @@ class Resource:
             "The demand cannot be less than zero.",\
             "The demand is greater than the total available."
         )
-        self._total -= allocated
+#        self._total -= allocated
         self._allocated = allocated
 
-
-
-    def __str__(self):
-        pass
-
-
-    def __repr__(self):
-        return f'Resource(name={self.name}, manufacturer={self.manufacturer}, '\
-                f'total={self.total}, allocated={self.allocated})'
-
-
-    @property
-    def total(self):
-        return self._total
-
-
-    @property
-    def allocated(self):
-        return self._allocated
-    
     
     @property
     def name(self):
@@ -46,60 +26,201 @@ class Resource:
 
     @property
     def manufacturer(self):
+        """
+        Returns:
+            str: the resource manufacturer
+        """
         return self._manufacturer
 
 
-    def claim(self, n):
-        if not validate_integer('claim', n, 0, self._total,\
-            custom_max_message="The demand is greater than the total available."):
-            self._total -= n
-            self._allocated += n
+    @property
+    def total(self):
+        """
+        Returns:
+            int: the total inventory count
+        """
+        return self._total
 
 
-    def freeup(self, n):
-        if not validate_integer('claim', n, 0, self._total,\
-            custom_max_message="The demand is greater than the total available."):
-            if n <= self._allocated:
-                self._allocated -= n
-                self._total += n
-
-
-    def died(self):
-        self._total = 0
-        self._allocated = 0
-
-
-    def purchased(self, n):
-        if not validate_integer('purchased', n):
-            self._total += n
-
+    @property
+    def allocated(self):
+        """
+        Returns:
+            int: number of resources in use
+        """
+        return self._allocated
+    
     
     @property
     def category(self):
-        return self.name.lower()
+        return type(self).__name__.lower()
 
-
-
-class CPU(Resource):
-
-    def __init__(self, cores, socket, power_watts, name, manufacturer, total=0, allocated=0):
-        super().__init__(name, manufacturer, total, allocated)
-        self.cores   = cores
-        self.socket  = socket
-        self.power_watts = power_watts
     
+    @property
+    def available(self):
+        return self.total - self.allocated
+
+    
+    def __str__(self):
+        return self.name
+
 
     def __repr__(self):
-        return f'CPU(name={self.name}, manufacturer={self.manufacturer},'\
-                f'total={self.total}, allocated={self.allocated}, '\
-                f'cores={self.cores}, socket={self.socket}, power_watts={self.power_watts})'
+        return f'{self.name} ({self.category} - {self.manufacturer}): '\
+               f'total={self.total}, allocated={self.allocated}'
 
-
-class Storage():
     
-    def __init__(self, capacity_GB):
-        self.capacity_GB = capacity_GB
+    def claim(self, num):
+        validate_integer(
+            'num', num, 1, self.available,
+            custom_max_message="The demand is greater than the total available."
+            )
+        #self._total -= num
+        self._allocated += num
+
+    
+    def freeup(self, num):
+        """
+        Retrun an inventory item to the available pool
+
+        Args:
+            num (int): Number of items to return (cannot exceed number in use)
+
+            Returns:
+                
+        """
+        validate_integer(
+            'num', num, 1, self._allocated,
+            custom_max_message="Cannot return more than allocated."
+            )
+        self._allocated -= num
+        #self._total += n
+
+
+    def died(self, num):
+        """
+        Number of items to deallocate and remove from the inventory pool
+        altogether
+
+        Args:
+            num (int): Number of items that have died
+
+            Returns:
+                
+        """
+        validate_integer(
+            'num', num, 1, self._allocated,
+            custom_max_message="Cannot retire more than allocated."
+            )
+        self._total -= num
+        self._allocated -= num
+
+
+    def purchased(self, num):
+        """
+        Number of items to deallocate and remove from the inventory pool
+        altogether
+
+        Args:
+            num (int): Number of items that have died
+
+            Returns:
+                
+        """
+        validate_integer('num', num, 1)
+        self._total += num
+    
+
+    
+class CPU(Resource):
+
+    def __init__(
+            self, name, manufacturer, total, allocated,
+            cores, socket, power_watts
+        ):
+        super().__init__(name, manufacturer, total, allocated)
+
+        validate_integer('cores', cores, 1)
+        validate_integer('power_watts', power_watts, 1)
+
+        self._cores  = cores
+        self._socket = socket
+        self._power_watts = power_watts
+    
+
+    @property
+    def cores(self):
+        """
+        The cores count for this CPU
+
+        Returns:
+            int
+        """
+        return self._cores
+
+
+    @property
+    def socket(self):
+        """
+        The socket type for this CPU
+
+        Returns:
+            str
+        """
+        return self._socket
+
+    
+    @property
+    def power_watts(self):
+        """
+        The rated wattage of this CPU
+
+        Returns:
+            int
+        """
+        return self._power_watts
+
+
+    def __repr__(self):
+        return f'{self.category}: {self.name} ({self.socket} - x{self.cores})'
+
+
+
+class Storage(Resource):
+    """
+    A base class for storage devices - probably not used directly
+    """
+    
+    def __init__(self, name, manufacturer, total, allocated, capacity_gb):
+        """
         
+        Args:
+            name (str): display name of resource
+            manufacturer (str): resource manufacturer
+            total (int): current total amount of resources
+            allocated (int): current count of in-use resources
+            capacity_gb (int): storage capacity (in GB)
+        """
+
+        super().__init__(name, manufacturer, total, allocated)
+        validate_integer('capacity_gb', capacity_gb, 1)
+        self._capacity_gb = capacity_gb
+        
+
+    @property
+    def capacity_gb(self):
+        """
+        Indicated the capacity (in GB) of the storage device
+
+        Returns:
+            int
+        """
+        return self._capacity_gb
+
+
+    def __repr__(self):
+        return f'{self.category}: {self.capacity_gb} GB'
+
 
 
 class HDD(Resource):
@@ -115,15 +236,19 @@ class SSD(Resource):
         super().__init__(name, manufacturer, total, allocated)
         self.interface = interface
         
-    def __repr__(self):
-        return f'SSD(interface={self.interface}, name={self.name}, manufacturer={self.manufacturer}, '\
-                f'total={self.total}, allocated={self.allocated})'
 
+
+
+resource =  {
+        'name': 'Core i7',
+        'manufacturer': 'Intel',
+        'total': 100,
+        'allocated': 50,
+        'cores': 16,
+        'socket': 'SATA',
+        'power_watts': 6
+    }
 
 if __name__ == '__main__':
-    r = Resource('Mouse X33', 'Razor', 5)
-    cpu = CPU(8, '22', 12, '13900K', 'INTEL', 20)
-    cpu.claim(10)
-    ssd = SSD('NVMe', 'EVO 860', 'Samsung', 70, 50)
-    print(repr(ssd))
-    print(repr(cpu))
+    print(repr(CPU(**resource)))
+    #print(CPU(**resource).__dict__)

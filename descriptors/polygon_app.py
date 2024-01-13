b1b0f252ebcad3b7568264054ef62b349e96152e
mohamadjalali <mohamadjalali324@gmail.com>
2024-01-13 13:48:03 +0330

    Adding several functions for more productivity

--- descriptors/polygon_app.py
+++ descriptors/polygon_app.py
@@ -5,9 +5,9 @@ from collections.abc import Sequence
 
 class Int:
     
-    def __init__(self, min_length=None, max_length=None):
-        self.min_length = min_length
-        self.max_length = max_length
+    def __init__(self, min_value=None, max_value=None):
+        self.min_value = min_value
+        self.max_value = max_value
 
 
     def __set_name__(self, owner_class, prop_name):
@@ -19,11 +19,11 @@ class Int:
         if not isinstance(value, int):
             raise ValueError("{self.name} must be an integer")
 
-        if self.min_length is not None and value < self.min_length:
-            raise ValueError(f"{self.name} least be at {self.min_length}")
+        if self.min_value is not None and value < self.min_value:
+            raise ValueError(f"{self.name} least be at {self.min_value}")
 
-        if self.max_length is not None and value > self.max_length:
-            raise ValueError(f"{self.name} value cannot be greater than {self.max_length}.")
+        if self.max_value is not None and value > self.max_value:
+            raise ValueError(f"{self.name} value cannot be greater than {self.max_value}.")
 
         instance.__dict__[self.name] = value
 
@@ -45,17 +45,23 @@ class Point2D:
     
    
     def __repr__(self):
-        return f'Person(x={self.x}, y={self.y})'
+        return f'Point2D(x={self.x}, y={self.y})'
 
 
     def __str__(self):
         return f'x={self.x}, y={self.y}'
 
 
+    def __eq__(self, other):
+        return isinstance(other, Point2D) and self.x == other.x and self.y == other.y
+
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
 
 
 
-class PolygonValidation:
+class Point2DSequence:
 
     def __init__(self, min_length=None, max_length=None):
         self.min_length = min_length
@@ -80,6 +86,10 @@ class PolygonValidation:
             if not isinstance(item, Point2D):
                 raise ValueError(f"Item at index {index} is not a Point instance.")
 
+        for index, item in enumerate(value):
+            if not isinstance(item, Point2D):
+                raise ValueError(f"Item at index {index} is not a Point2D instance")
+
         instance.__dict__[self.name] = list(value)
 
 
@@ -94,21 +104,21 @@ class PolygonValidation:
 
 
 class Polygon:
-    vertices = PolygonValidation(min_length=3, max_length=4)
+    vertices = Point2DSequence(min_length=3)
 
     def __init__(self, *vertices):
         self.vertices = vertices
 
 
-    def append(self, value):
-        if not isinstance(value, Point2D):
+    def append(self, pt):
+        if not isinstance(pt, Point2D):
             raise ValueError('Can only append Point2D instances')
         
         max_length = type(self).vertices.max_length
         if max_length is not None and len(self.vertices) >= max_length:
             raise ValueError(f"Vetices length is at max ({max_length})")
         
-        self.vertices.append(value)
+        self.vertices.append(pt)
     
     
     def __iadd__(self, other):
@@ -116,7 +126,26 @@ class Polygon:
         return self
 
 
+    def __len__(self):
+        return len(self.vertices)
+
+
+    def __getitem__(self, index):
+        return self.vertices[index]
+
+
     def __contains__(self, pt):
-        pass
+        return pt in self.vertices
 
 
+
+class Triangle(Polygon):
+    vertices = Point2DSequence(min_length=3, max_length=3)
+
+
+class Rectangle(Polygon):
+    vertices = Point2DSequence(min_length=4, max_length=4)
+
+
+#p = Polygon(Point2D(10, 10), Point2D(5, 9), Point2D(15, 15))
+#print(p.vertices)
